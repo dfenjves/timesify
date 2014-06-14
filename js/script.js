425adf96d8ee4bcc69ae6896e98f3ef9ed9959a6
Esteban Pastorino <ejpastorino@gmail.com>
2014-06-14 13:39:17 -0400

    bigger if

--- js/script.js
+++ js/script.js
@@ -51,21 +51,21 @@ function article_content(data){
   $("[data-role=fake-news]").html(content)
 }
 
-$.ajax({
-  url: "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp",
-  data: {
-    "sort": "newest",
-    "api-key": "7fcada8d2ec5b00ecb81f0cec14db29a:7:69489929",
-    "callback": "svc_search_v2_articlesearch",
-    //"fl": "snippet,lead_paragraph,abstract,multimedia,headline,byline,web_url,section_name,news_desk",
-    "fq": 'source:("The New York Times") AND section_name:("Business" "New York" "U.S." "World") AND type_of_material:("News")'
-  },
-  type: "GET",
-  dataType: "script"
-});
-
 var url = getParameterByName("url");
 if(url){
+  $.ajax({
+    url: "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp",
+    data: {
+      "sort": "newest",
+      "api-key": "7fcada8d2ec5b00ecb81f0cec14db29a:7:69489929",
+      "callback": "svc_search_v2_articlesearch",
+      //"fl": "snippet,lead_paragraph,abstract,multimedia,headline,byline,web_url,section_name,news_desk",
+      "fq": 'source:("The New York Times") AND section_name:("Business" "New York" "U.S." "World") AND type_of_material:("News")'
+    },
+    type: "GET",
+    dataType: "script"
+  });
+
   $.ajax({
     url: "http://api.embed.ly/1/extract",
     data: {
